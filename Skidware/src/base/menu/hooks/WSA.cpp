d687a0d9e51db9ee6554a6cf930e78303f5386b4
DiamondCoder1000 <78707555+CheatMaker@users.noreply.github.com>
2023-08-14 20:46:21 -0700

    adsad

--- Skidware/src/base/menu/hooks/WSA.cpp
+++ Skidware/src/base/menu/hooks/WSA.cpp
@@ -4,11 +4,12 @@
 #include "../../util/logger.h"
 #include "../../../../ext/minhook/minhook.h"
 #include "../../moduleManager/modules/player/blink.h"
-
+#include "../../eventManager/EventManager.hpp"
+#include "../../eventManager/events/EventPacketSend.h"
 int(__stdcall* g_origWSASend)(SOCKET, LPWSABUF, DWORD, LPDWORD, DWORD, LPWSAOVERLAPPED, LPWSAOVERLAPPED_COMPLETION_ROUTINE);
 int __stdcall WSASendHook(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesSent, DWORD dwFlags, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine)
 {
-	
+	EventManager::getInstance().call(EventPacketSend((const char*)lpBuffers));
 	while (Blink::getInstance()->getToggle()) {
 		
 		Sleep(1);
@@ -16,8 +17,20 @@ int __stdcall WSASendHook(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPD
 	return g_origWSASend(s, lpBuffers, dwBufferCount, lpNumberOfBytesSent, dwFlags, lpOverlapped, lpCompletionRoutine);
 }
 
+int(__stdcall* g_origWSARecev)(SOCKET, LPWSABUF, DWORD, LPDWORD, DWORD, LPWSAOVERLAPPED, LPWSAOVERLAPPED_COMPLETION_ROUTINE);
+int __stdcall WSARecevHook(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesSent, DWORD dwFlags, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine)
+{
+
+	while (Blink::getInstance()->getToggle()) {
+
+		Sleep(1);
+	}
+	return g_origWSASend(s, lpBuffers, dwBufferCount, lpNumberOfBytesSent, dwFlags, lpOverlapped, lpCompletionRoutine);
+}
+
 void Menu::Hook_WSA() {
 	MH_CreateHookApi(L"Ws2_32.dll", "WSASend", WSASendHook, (void**)&g_origWSASend);
+	MH_CreateHookApi(L"Ws2_32.dll", "WSARecv", WSARecevHook, (void**)&g_origWSARecev);
 	MH_EnableHook(MH_ALL_HOOKS);
 }
 
